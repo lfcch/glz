b0ddf1f7adaa8b11a730f165dac9c0bf2af5ec24
John D. Gaffney <john.gaffney@isobar.com>
2017-06-28 18:40:31 -0400

    Restarting project

---
+++ glz/glz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  glz
+//
+//  Created by John D. Gaffney on 6/28/17.
+//  Copyright © 2017 gffny.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.blue
+    }
+    
+}
+
